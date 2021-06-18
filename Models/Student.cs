9d3e476b8be66ac4f28a2c96cbd0c0a9cca8a2d6
Enigmatron <10167806+Enigmatron@users.noreply.github.com>
2021-06-18 14:57:10 -0500

    changes

--- Models/Student.cs
+++ Models/Student.cs
@@ -7,11 +7,23 @@ namespace RazorPagesStudent.Models
     public class Student
     {
         public int ID { get; set; }
+        //
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Title { get; set; }
+        //
         [Display(Name = "Release Date")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+        //
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [Required]
+        [StringLength(30)]
         public string Genre { get; set; }
+        //
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
         [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
